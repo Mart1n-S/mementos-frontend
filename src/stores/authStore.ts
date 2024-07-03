c7d8c2b2eeeccb77806c66c44eb0e86a13cdb462
Martin <martinsimongo@gmail.com>
2024-07-03 22:24:25 +0200

    ajout de la fonctionnalité de déconnexion

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,30 +1,46 @@
-// stores/authStore.ts
 import { defineStore } from 'pinia';
-import type { User } from '@/models/User';
+import { ref } from 'vue';
+import axios from '@/modules/axios';
+import { useRouter } from 'vue-router';
 
-export const useAuthStore = defineStore('auth', {
-    state: () => ({
-        isAuthenticated: false,
-        user: null as User | null
-    }),
-    actions: {
-        login(user: User) {
-            this.isAuthenticated = true;
-            this.user = user;
-        },
-        logout() {
-            this.isAuthenticated = false;
-            this.user = null;
-        },
-        checkAuth() {
+export const useAuthStore = defineStore('auth', () => {
+    const router = useRouter();
+    const isAuthenticated = ref(false);
+    const user = ref<{ name?: string; email?: string } | null>(null);
+
+    function login(userData: { name: string; email: string }) {
+        isAuthenticated.value = true;
+        user.value = userData;
+    }
+
+    async function logout() {
+        try {
             const token = localStorage.getItem('access_token');
-            if (token) {
-                this.isAuthenticated = true;
-                // Optionally, set the user data
-                // this.user = fetchUserDataFromToken(token);
-            } else {
-                this.isAuthenticated = false;
-            }
+            await axios.post('/logout', {}, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            isAuthenticated.value = false;
+            user.value = null;
+            localStorage.removeItem('access_token');
+            router.push('/');
+        } catch (error) {
+            console.error('Failed to logout:', error);
         }
     }
+
+    function checkAuth() {
+        const token = localStorage.getItem('access_token');
+        if (token) {
+            isAuthenticated.value = true;
+            // Optionally, fetch and set the user data here
+            // Example: user.value = { name: 'John Doe', email: 'john@example.com' };
+        } else {
+            isAuthenticated.value = false;
+        }
+    }
+
+    return { isAuthenticated, user, login, logout, checkAuth };
 });
+
