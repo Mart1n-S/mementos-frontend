8b7b5938221b4f9acbe019b59dcf809a302fbcd1
Martin <martinsimongo@gmail.com>
2024-07-07 21:20:43 +0200

    Mise en place du service worker pour la gestion du cache

--- src/stores/categorieStore.ts
+++ src/stores/categorieStore.ts
@@ -7,46 +7,15 @@ export const useCategorieStore = defineStore('categorie', () => {
     const categories = ref<Categorie[]>([]);
     const errorMessage = ref<string | null>(null);
 
-    function getLocalStorageCategories() {
-        const categoriesString = localStorage.getItem('categories');
-        return categoriesString ? JSON.parse(categoriesString) : null;
-    }
-
-    function setLocalStorageCategories(categories: Categorie[]) {
-        localStorage.setItem('categories', JSON.stringify(categories));
-        localStorage.setItem('categoriesTimestamp', Date.now().toString());
-    }
-
     async function fetchCategories() {
-        const localCategories = getLocalStorageCategories();
-        const lastFetch = localStorage.getItem('categoriesTimestamp');
-        const now = Date.now();
-
-        // Vérifier si les catégories locales sont présentes et si elles sont récentes (par exemple, moins de 1 heure)
-        if (localCategories && lastFetch && now - parseInt(lastFetch) < 3600000) {
-            categories.value = localCategories;
-        } else {
-            try {
-                const response = await axios.get<Categorie[]>('/categories');
-                categories.value = response.data;
-                setLocalStorageCategories(response.data);
-            } catch (error: any) {
-                errorMessage.value = 'Erreur lors de la récupération des catégories';
-                console.error('Erreur lors de la récupération des catégories:', error);
-            }
-        }
-    }
-
-    async function refreshCategories() {
         try {
             const response = await axios.get<Categorie[]>('/categories');
             categories.value = response.data;
-            setLocalStorageCategories(response.data);
         } catch (error: any) {
-            errorMessage.value = 'Erreur lors de la mise à jour des catégories';
-            console.error('Erreur lors de la mise à jour des catégories:', error);
+            errorMessage.value = 'Erreur lors de la récupération des catégories';
+            console.error('Erreur lors de la récupération des catégories:', error);
         }
     }
 
-    return { categories, errorMessage, fetchCategories, refreshCategories };
+    return { categories, errorMessage, fetchCategories };
 });
